ec1ca7b4b3c5eafa8448625affccb0f161116846
Owais Shaikh <theowaiszoot@gmail.com>
2022-06-27 20:37:22 +0530

    Initial Commit

---
+++ Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
